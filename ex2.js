2ecc1b7b1321c073231a5d63e16ef672a81dfed7
Warllei Martins <warllei.s.martins@hotmail.com>
2024-01-26 04:48:43 -0300

    mais exercicios

--- ex2.js
+++ ex2.js
@@ -1,53 +1,96 @@
 //ex 1
 
-let itens = ["item1", "item2", "item3", "item4", "item5"];
-const slice = itens.slice(0, 3);
-const newItens = [];
-const replace = slice.forEach((element, index) =>
-  newItens.push(element.replace("item", ""))
-);
-itens = newItens;
-console.log(itens);
+// let itens = ["item1", "item2", "item3", "item4", "item5"];
+// const slice = itens.slice(0, 3);
+// const newItens = [];
+// const replace = slice.forEach((element, index) =>
+//   newItens.push(element.replace("item", ""))
+// );
+// itens = newItens;
+// console.log(itens);
 
 //ex 2
 
+// async function chamandoTarefaAssincrona(tarefasAssincronas) {
+//   if (!Array.isArray(tarefasAssincronas)) {
+//     throw new Error("As tarefas devem ser fornecidas em um array.");
+//   }
 
-async function chamandoTarefaAssincrona(tarefasAssincronas) {
-  if (!Array.isArray(tarefasAssincronas)) {
-    throw new Error("As tarefas devem ser fornecidas em um array.");
+//   for (const tarefaAssincrona of tarefasAssincronas) {
+//     try {
+//       await tarefaAssincrona();
+//       console.log("Tarefa concluída com sucesso.");
+//     } catch (error) {
+//       console.error("Erro durante a execução da tarefa:", error);
+//     }
+
+//     await new Promise((resolve) => setTimeout(resolve, 2000));
+//   }
+// }
+
+// const tarefa1 = async () => {
+//   console.log("Iniciando tarefa 1...");
+//   await new Promise((resolve) => setTimeout(resolve, 2000));
+//   console.log("Tarefa 1 concluída...");
+// };
+
+// const tarefa2 = async () => {
+//   console.log("Iniciando tarefa 2...");
+//   await new Promise((resolve) => setTimeout(resolve, 2000));
+//   console.log("Tarefa 2 concluída...");
+// };
+
+// const arrayDeTarefas = [tarefa1, tarefa2];
+
+// chamandoTarefaAssincrona(arrayDeTarefas);
+
+//ex 3
+
+// (async () => {
+//   let data = await fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then((res) => res.json())
+//     .then((response) => response);
+//   console.log(data);
+// })();
+
+//ex 4
+
+class Subject {
+  constructor() {
+    this.observers = [];
   }
 
-  for (const tarefaAssincrona of tarefasAssincronas) {
-    if (typeof tarefaAssincrona !== "function") {
-      throw new Error("Cada tarefa deve ser uma função assíncrona.");
-    }
+  addObserver(observer) {
+    this.observers.push(observer);
+  }
 
-    const novaTarefa = async () => {
-      try {
-        await tarefaAssincrona();
-        console.log("Tarefa concluída com sucesso.");
-      } catch (error) {
-        console.error("Erro durante a execução da tarefa:", error);
-      }
-    };
+  removeObserver(observer) {
+    this.observers = this.observers.filter(obs => obs !== observer);
+  }
 
-    await novaTarefa();
-    await new Promise(resolve => setTimeout(resolve, 2000));
+  notifyObservers() {
+    this.observers.forEach(observer => {
+      observer.update();
+    });
   }
 }
 
-const tarefa1 = async () => {
-  console.log("Iniciando tarefa 1...");
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-  console.log("Tarefa 1 concluída...");
-};
+class Observer {
+  constructor(name) {
+    this.name = name;
+  }
 
-const tarefa2 = async () => {
-  console.log("Iniciando tarefa 2...");
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-  console.log("Tarefa 2 concluída...");
-};
+  update() {
+    console.log(`Observer ${this.name} foi notificado sobre a mudança.`);
+  }
+}
 
-const arrayDeTarefas = [tarefa1, tarefa2];
+const subject = new Subject();
 
-chamandoTarefaAssincrona(arrayDeTarefas);
+const observer1 = new Observer('1');
+const observer2 = new Observer('2');
+
+subject.addObserver(observer1);
+subject.addObserver(observer2);
+subject.notifyObservers();
+
