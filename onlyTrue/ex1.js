48ddd9630d3240a562b2789483618b9284a3d357
Warllei Martins <warllei.s.martins@hotmail.com>
2024-04-09 22:41:51 -0300

    more exercises

---
+++ onlyTrue/ex1.js
@@ -0,0 +1,18 @@
+const obj = [
+    { item1: true, item2: true, item3: true },
+    { item1: true, item2: false, item3: true },
+    { item1: true, item2: true, item3: true },
+    { item1: true, item2: true, item3: true },
+]
+
+const filter = obj.filter((obj) => Object.values(obj).filter(Boolean).length === 3)
+console.log(filter)
+
+
+const result = obj.filter((item) => {
+    const values = Object.values(item);
+    return values.length === 3 && values.every(Boolean);
+});
+
+console.log(result);
+
